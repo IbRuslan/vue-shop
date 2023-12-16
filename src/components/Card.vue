77e8a3243971c440f5584640c7ea602fd68155aa
IbRuslan <ribragimov2003@gmail.com>
2023-12-16 22:20:37 +0500

    verstka cardlist

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,26 @@
+<script setup>
+defineProps({
+  title: String,
+  imageUrl: String,
+  price: Number,
+  isFavorite: Boolean,
+  isAdded: Boolean,
+  onClickAdd: Function,
+  onClickFavorite: Function
+})
+</script>
+
 <template>
-  <div class="relative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
-    <img class="absolute top-8 left-8" src="/like-2.svg" alt="like">
-    <img src="/sneakers/sneakers-1.jpg" alt="sneaker">
-    <p class="mt-2">Мужские кроссовки Nike Blazer Mid Suede</p>
+  <div class="relative bg-white m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
+    <img class="absolute top-8 left-8" :src="isFavorite ? '/like-2.svg' : '/like-1.svg' " @click="onClickFavorite" alt="like">
+    <img :src="imageUrl" alt="sneaker">
+    <p class="mt-2">{{title}}</p>
     <div class="flex justify-between mt-5">
       <div class="flex flex-col">
         <span class="text-slate-400">Цена</span>
-        <b>1205 руб.</b>
+        <b>{{price}} руб.</b>
       </div>
-      <img src="/plus.svg" alt="plus">
+      <img :src="!isAdded ? '/plus.svg' : '/checked.svg' " @click="onClickAdd" alt="plus">
     </div>
   </div>
 </template>
---
+++ src/components/CardList.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Card from "@/components/Card.vue";
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-2">
+    <Card imageUrl="/sneakers/sneakers-1.jpg" title="Мужские кроссовки" :price="1205" is-added is-favorite/>
+    <Card imageUrl="/sneakers/sneakers-1.jpg" title="Мужские кроссовки" :price="1205"/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+  </div>
+</template>
