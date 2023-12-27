12749ffe175d7aa906af7759f065e5620001b8c7
IbRuslan <ribragimov2003@gmail.com>
2023-12-27 15:53:09 +0500

    add axios, api, filters by sort and search

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,48 @@
 <script setup>
+import {onMounted, reactive, ref, watch} from "vue";
+import axios from "axios";
+
 import Header from "@/components/Header.vue";
 import CardList from "@/components/CardList.vue";
 import Drawer from "@/components/Drawer.vue";
+
+const items = ref([])
+const filters = reactive({
+  sortBy: '',
+  searchQuery: ''
+})
+
+const onChangeSelect = (e) => {
+  filters.sortBy = e.target.value
+}
+
+const onChangeSearchInput = (e) => {
+  if (e.target.value.trim() !== '') {
+    filters.searchQuery = e.target.value.trim()
+  }
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy,
+    }
+    if(filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const {data} = await axios.get('https://604781a0efa572c1.mokky.dev/items', {
+      params
+    })
+
+    items.value = data
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -15,20 +56,20 @@ import Drawer from "@/components/Drawer.vue";
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
 
         <div class="flex flex-col gap-4">
-          <select class="py-2 px-3 border rounded-md outline-none cursor-pointer">
-            <option>по названию</option>
-            <option>По цене (дешевые)</option>
-            <option>По цене (дорогие)</option>
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none cursor-pointer">
+            <option value="name">по названию</option>
+            <option value="price">По цене (дешевые)</option>
+            <option value="-price">По цене (дорогие)</option>
           </select>
 
           <div class="relative">
             <img class="absolute left-4 top-3" src="/search.svg" />
-            <input class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400" placeholder="Поиск" />
+            <input @input="onChangeSearchInput" class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400" placeholder="Поиск" />
           </div>
         </div>
       </div>
 
-      <CardList/>
+      <CardList :items="items" />
     </div>
 
   </div>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,14 +1,27 @@
 <script setup>
 import Card from "@/components/Card.vue";
+
+defineProps({
+  items: Array
+})
+
+const onClickAdd = () => {
+  alert('Добавить')
+}
+
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-2">
-    <Card imageUrl="/sneakers/sneakers-1.jpg" title="Мужские кроссовки" :price="1205" is-added is-favorite/>
-    <Card imageUrl="/sneakers/sneakers-2.jpg" title="Мужские кроссовки" :price="1205"/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
+    <Card
+        :key="item.id"
+        v-for="item in items"
+        :imageUrl="item.imageUrl"
+        :title="item.title"
+        :price="item.price"
+        :on-click-add="onClickAdd"
+        :is-added="true"
+        :is-favorite="false"
+    />
   </div>
 </template>
